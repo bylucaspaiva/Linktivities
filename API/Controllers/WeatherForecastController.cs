015e5857234d05083179b811702a40017cc88689
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-22 20:48:05 -0300

    feat: add activity class to Domain

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("[controller]")]//www.localhost:5000/wheaterforecast
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
