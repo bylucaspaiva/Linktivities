60d6e3a459d7acf1003d1945be240dd9f54c96ef
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-23 12:46:50 -0300

    feat: add ActivitiesController

---
+++ API/Controllers/ActivitivitiesController.cs
@@ -0,0 +1,14 @@
+﻿using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
