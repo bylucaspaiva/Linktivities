32fc06d88a87e2f30ab7542c501285d6c6d0e3ed
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-25 21:48:57 -0300

    Feat: implement method required by interface and creates a constructor

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Domain;
+using MediatR;
+using Persistence;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,24 @@ namespace Application.Activities
 {
     public class Edit
     {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
