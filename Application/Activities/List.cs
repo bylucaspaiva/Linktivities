770b7a0f9c5697475eacfac394c6ae8a0e2df165
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-30 22:02:19 -0300

    feat: adding cancelation token to list controller

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,6 +1,7 @@
 ﻿using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 using System;
 using System.Collections.Generic;
@@ -17,13 +18,28 @@ public class List
     public class Handler : IRequestHandler<Query, List<Activity>>
     {
         private readonly DataContext _context;
+        private readonly ILogger<List> _logger;
 
-        public Handler(DataContext context)
+        public Handler(DataContext context, ILogger<List> logger)
         {
             _context = context;
+            _logger = logger;
         }
         public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
         {
+            try
+            {
+                for(var i = 0; i < 10; i++)
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    await Task.Delay(1000, cancellationToken);
+                    _logger.LogInformation($"Task {i} has completed");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("Task was cancelled");
+            }
             return await _context.Activities.ToListAsync();
         }
     }
