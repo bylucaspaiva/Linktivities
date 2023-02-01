6e4666ee82d18259a8864930d4644149b2829d28
Lucas Paiva <lucasp4iva@gmail.com>
2023-02-01 15:52:58 -0300

    feat: add ActivityDetails and its implementations

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { Grid, GridColumn, List } from "semantic-ui-react";
+import { Grid, GridColumn} from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
 import ActivityList from "./ActivityList";
 
 interface Props {
   activities: Activity[];
 }
 
-export default function ActivityDashboard({activities}: Props){
+export default function ActivityDashboard({ activities }: Props) {
+  console.log("act:",activities[0])
   return (
-    <Grid.Column width={10}>
-      <ActivityList activities={activities} />
-    </Grid.Column>
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList activities={activities} />
+      </Grid.Column>
+      <GridColumn width={6}>
+        {activities[0] &&
+        <ActivityDetails activity={activities[0]} />}
+      </GridColumn>
+    </Grid>
+
   )
 }
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Button, Card, Image } from 'semantic-ui-react'
+import { Activity } from '../../../app/models/activity'
 
-const ActivityDetails = () => {
+interface Props {
+  activity: Activity
+}
+
+const ActivityDetails = ({activity}: Props) => {
   return (
-    <div>ActivityDetails</div>
+    <Card>
+    <Image src={`/assets/categoryImages/${activity.category}.jpg`}  />
+    <Card.Content>
+      <Card.Header>{activity.title}</Card.Header>
+      <Card.Meta>
+        <span className='date'>{activity.date}</span>
+      </Card.Meta>
+      <Card.Description>
+        {activity.description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <Button.Group widths={2} >
+        <Button basic color='blue' content='Edit'/>
+        <Button basic color='grey' content='Cancel'/>
+      </Button.Group> 
+    </Card.Content>
+  </Card>
   )
 }
 
-export default ActivityDetails
+export default ActivityDetails;
