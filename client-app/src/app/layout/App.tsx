48505ed368d412a5d00ec55d4b437a7077e6081a
Lucas Paiva <lucasp4iva@gmail.com>
2023-02-01 00:26:13 -0300

    feat: pass props to activityDashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,28 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-        console.log(response.data);
-        setActivities(response.data);
-      })
-  },[])
+      console.log(response.data);
+      setActivities(response.data);
+    })
+  }, [])
 
   return (
-    <div className="App">
+    <>
       <NavBar />
-      <List>
-        {activities.map(activity => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+      <Container style={{marginTop: '7em'}}>
+       <ActivityDashboard activities={activities}/> 
+      </Container>
+    </>
   );
 }
 
