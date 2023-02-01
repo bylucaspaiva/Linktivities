7ea67bd6eb246a42fb7cb9a162ff0868495f53b5
Lucas Paiva <lucasp4iva@gmail.com>
2023-02-01 00:32:14 -0300

    feat: add ActivityList

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -6,11 +6,11 @@ interface Props {
   activities: Activity[];
 }
 
-export default function ActivityDashboard(props: Props){
+export default function ActivityDashboard({activities}: Props){
   return (
     <Grid.Column width={10}>
       <List>
-          {props.activities.map(activity => (
+          {activities.map(activity => (
             <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+  activities: Activity[];
+}
+
+const ActivityList = ({activities}: Props) => {
+  return (
+    <></>
+  )
+}
+
+export default ActivityList;
