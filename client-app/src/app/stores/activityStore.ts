618cf388b939eda467380b03b36fdc920cc49534
Lucas Paiva <lucasp4iva@gmail.com>
2023-02-13 22:49:15 -0300

    feat: add createActivity action

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,6 +1,7 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
+import {v4 as uuid} from "uuid";
 
 export default class ActivityStore {
   activities: Activity[] = [];
@@ -47,5 +48,24 @@ export default class ActivityStore {
 
   closeForm = () => {
     this.editMode = false;
+  }
+
+  createActivity = async (activity: Activity) => {
+    this.loading = true;
+    activity.id = uuid();
+    try {
+      await agent.Activities.create(activity);
+      runInAction(() => {
+        this.activities.push(activity);
+        this.selectedActivity = activity;
+        this.editMode = false;
+        this.loading = false; 
+      })
+    }catch (error) {
+      console.log(error);
+      runInAction(() => {
+        this.editMode = false;
+      })
+    }
   }
 }
