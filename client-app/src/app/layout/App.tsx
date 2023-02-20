9327538635d5e1bb1d33a7c5daa62de6d3716432
Lucas Paiva <lucasp4iva@gmail.com>
2023-02-20 09:18:19 -0300

    feat: add children routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   const {activityStore} = useStore();
@@ -22,7 +22,7 @@ function App() {
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard />
+        <Outlet />
       </Container>
     </>
   );
---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+const HomePage = () => {
+  return (
+    <Container style={{marginTop: '7em'}}>
+      <div>HomePage</div>
+    </Container>
+  )
+}
+
+export default HomePage
