a6000d88e7754e3a287f107aa8aaac34ed5541e8
Lucas Paiva <lucasp4iva@gmail.com>
2023-02-19 23:05:54 -0300

    refactor: uses router to display app

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css'
-import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import "./app/layout/styles.css"
 import { store, StoreContext } from './app/stores/store';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <RouterProvider router={router} />
   </StoreContext.Provider>
 );
 
