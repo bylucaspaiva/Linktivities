cb341a4b9e53d500cc693fa01a3a018bb8fdd064
Lucas Paiva <lucasp4iva@gmail.com>
2023-02-23 21:16:37 -0300

    refactor: change how item list is displayed

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Item, Label, Segment } from 'semantic-ui-react'
+import { Button, Icon, Item, Label, Segment } from 'semantic-ui-react'
 import { Activity } from '../../../app/models/activity'
 import { useStore } from '../../../app/stores/store';
 
@@ -26,10 +26,26 @@ const ActivityListItem = ({activity} : Props) => {
           <Item>
             <Item.Image size='tiny' circular src='/assets/user.png' />
             <Item.Content>
-              <Item.Header as={Link} to={`/activities/${activity.id}`}>{activity.title}</Item.Header>
+              <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                {activity.title}
+              </Item.Header>
+              <Item.Description>Hosted by Lucas</Item.Description>
             </Item.Content>
           </Item>
         </Item.Group>
+      </Segment>
+      <Segment>
+        <span>
+          <Icon name='clock' /> {activity.date}
+          <Icon name='marker' /> {activity.venue}
+        </span>
+      </Segment>
+      <Segment secondary>
+        Attendees go here
+      </Segment>
+      <Segment clearing>
+        <span>{activity.description}</span>
+        <Button as={Link} to={`/activities/${activity.id}`} color='teal' floated='right' content='View' />
       </Segment>
     </Segment.Group>
   )
