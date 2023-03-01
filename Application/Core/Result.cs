364230425e9fc868f92ebdd873fd51e0967ff4bf
Lucas Paiva <lucasp4iva@gmail.com>
2023-02-28 21:09:39 -0300

    feat: add logic

--- Application/Core/Result.cs
+++ Application/Core/Result.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Application.Core
 {
-    internal class Result
+    public class Result<T>
     {
+        public bool IsSuccess { get; set; }
+        public T Value { get; set; }
+        public string Error { get; set; }
+
+        public static Result<T> Success(T value) => new Result<T> { IsSuccess = true, Value = value };
+
+        public static Result<T> Failure(string Error) => new Result<T> { IsSuccess = false, Error = Error };
     }
 }
