8580012ba34d785d20a9a5e1158d817bcfcf0ae2
Lucas Paiva <lucasp4iva@gmail.com>
2023-03-04 17:58:03 -0300

    feat: add exception middleware class

--- Application/Core/AppException.cs
+++ Application/Core/AppException.cs
@@ -1,6 +1,16 @@
 ﻿namespace Application.Core
 {
-    internal class AppException
+    public class AppException
     {
+        public AppException(int statusCode, string message, string details = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Details = details;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
     }
 }
