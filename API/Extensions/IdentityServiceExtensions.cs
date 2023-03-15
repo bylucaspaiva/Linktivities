90facb18310fb44aa72b95cca3a43850ce6dafa0
Lucas Paiva <lucasp4iva@gmail.com>
2023-03-15 00:03:45 -0300

    feat: configuring identity in the startup class

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,6 +1,19 @@
-﻿namespace API.Extensions
+﻿using Domain;
+using Persistence;
+
+namespace API.Extensions
 {
-    public class IdentityServiceExtensions
+    public static class IdentityServiceExtensions
     {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config) 
+        {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = true;
+            }).AddEntityFrameworkStores<DataContext>();
+            services.AddAuthentication();
+
+            return services;
+        }
     }
 }
