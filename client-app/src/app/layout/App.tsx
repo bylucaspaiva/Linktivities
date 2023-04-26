7d9ffad53ddec1dda0c0912d852b7ecc31bab392
Lucas Paiva <lucasp4iva@gmail.com>
2023-04-26 18:53:13 -0300

    feat: persisting token on client-side

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { observer } from 'mobx-react-lite';
@@ -6,16 +6,31 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import { ToastContainer } from 'react-toastify';
 import { setupResponseInterceptor } from '../api/agent';
+import { useStore } from '../stores/store';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const location = useLocation();
   const navigate = useNavigate();
+  const {commonStore, userStore} = useStore();
+
+  useEffect(() => {
+    if(commonStore.token){
+      userStore.getUser().finally(() => commonStore.setAppLoaded())
+    } else {
+      commonStore.setAppLoaded()
+    }
+  }, [commonStore, userStore])
+
 
   const [isLoaded, setIsLoaded] = useState(false)
     if (!isLoaded) {
         setIsLoaded(true)
     setupResponseInterceptor(navigate)
     }
+
+  if(!commonStore.appLoaded) return <LoadingComponent content='Loading App...' />
+
 
   return (
     <>
