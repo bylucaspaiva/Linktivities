df290e0dc24957eec1e8666d869a59dd70a0e64d
Lucas Paiva <lucasp4iva@gmail.com>
2023-05-02 19:44:56 -0300

    feat: assuring that attendees is not a null object

--- Infrastructure/Security/UserAccessor.cs
+++ Infrastructure/Security/UserAccessor.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace Infrastructure.Security
 {
-    internal class UserAccessor
+    public class UserAccessor : IUserAccessor
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserAccessor(IHttpContextAccessor httpContextAccessor) 
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string GetUserName()
+        {
+            return _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+        }
     }
 }
