fecc60bbdd06532c030a250c0ca8fdab71572377
Lucas Paiva <lucasp4iva@gmail.com>
2023-05-03 17:43:35 -0300

    Feat: Configuring autoMapper profiles

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -14,6 +14,13 @@ public class MappingProfiles : Profile
     public MappingProfiles()
     {
         CreateMap<Activity, Activity>();
-        CreateMap<Activity, ActivityDTO>();
+        CreateMap<Activity, ActivityDTO>()
+            .ForMember(d => d.HostUserName, o => o.MapFrom(s => s.Attendees
+                .FirstOrDefault(x => x.IsHost).AppUser.UserName));
+        CreateMap<ActivityAttendee, Profiles.Profile>()
+            .ForMember(d => d.DisplayName, o => o.MapFrom((s => s.AppUser.DisplayName)))
+            .ForMember(d => d.Username, o => o.MapFrom((s => s.AppUser.UserName)))
+            .ForMember(d => d.Bio, o => o.MapFrom((s => s.AppUser.Bio)));
+
     }
 }
