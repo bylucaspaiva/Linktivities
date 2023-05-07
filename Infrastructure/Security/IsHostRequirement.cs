77c559fad53e1f9c6deafe9e64e63447ebc1a907
Lucas Paiva <lucasp4iva@gmail.com>
2023-05-07 20:41:52 -0300

    feat: add handleRequirement

--- Infrastructure/Security/IsHostRequirement.cs
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+
+using Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,13 +17,30 @@ public class IsHostRequirement : IAuthorizationRequirement
 
 public class IsHostRequiremntHandler : AuthorizationHandler<IsHostRequirement>
 {
-    public IsHostRequiremntHandler()
-    {
+    private readonly DataContext _dbContext;
+    private readonly IHttpContextAccessor _httpContextAcessor;
 
+    public IsHostRequiremntHandler(DataContext dbContext, IHttpContextAccessor httpContextAcessor)
+    {
+        _dbContext = dbContext;
+        _httpContextAcessor = httpContextAcessor;
     }
 
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
     {
-        throw new NotImplementedException();
+        var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (userId == null) return Task.CompletedTask;
+
+        var activityId = Guid.Parse(_httpContextAcessor.HttpContext?.Request.RouteValues
+            .SingleOrDefault(x => x.Key == "id").Value?.ToString());
+
+        var attendee = _dbContext.ActivityAttendees.FindAsync(userId, activityId).Result;
+
+        if (attendee == null) return Task.CompletedTask;
+
+        if (attendee.IsHost) context.Succeed(requirement);
+
+        return Task.CompletedTask;
     }
 }
