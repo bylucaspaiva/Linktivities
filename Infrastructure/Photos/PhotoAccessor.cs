189236d3853b78781c128b4b124569b6d6d03d23
Lucas Paiva <lucasp4iva@gmail.com>
2023-05-19 17:11:48 -0300

    feat: add photoAcessor class and interface and inject to serverExtensions

---
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces;
+using Application.Photos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Photos
+{
+    public class PhotoAccessor : IPhotoAccessor
+    {
+        public PhotoAccessor(IOptions<CloudinarySettings> config)
+        {
+
+        }
+
+        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeletePhoto(string publicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
