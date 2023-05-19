ec3d196a490655f8afa7da63a5f6601292974d55
Lucas Paiva <lucasp4iva@gmail.com>
2023-05-19 17:35:49 -0300

    feat: implements add photo logic

--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Interfaces;
 using Application.Photos;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using System;
@@ -12,14 +14,44 @@ namespace Infrastructure.Photos
 {
     public class PhotoAccessor : IPhotoAccessor
     {
+        private readonly Cloudinary _cloudinary;
         public PhotoAccessor(IOptions<CloudinarySettings> config)
         {
+            var account = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+            );
 
+            _cloudinary = new Cloudinary( account );
         }
 
-        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        public async Task<PhotoUploadResult> AddPhoto(IFormFile file)
         {
-            throw new NotImplementedException();
+            if(file.Length > 0)
+            {
+                await using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill")
+                };
+
+                var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+                if(uploadResult.Error != null)
+                {
+                    throw new Exception(uploadResult.Error.Message);
+                }
+
+                return new PhotoUploadResult
+                {
+                    PublicId = uploadResult.PublicId,
+                    Url = uploadResult.SecureUrl.ToString()
+                };
+            }
+
+            return null;
         }
 
         public Task<string> DeletePhoto(string publicId)
