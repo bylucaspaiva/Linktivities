117841a1f62f1e292eadc4ff3fceb3bacadbd88c
Lucas Paiva <lucasp4iva@gmail.com>
2023-05-19 19:39:29 -0300

    feat: implements add photo handler

--- Domain/Photo.cs
+++ Domain/Photo.cs
@@ -8,7 +8,7 @@ namespace Domain
 {
     public class Photo
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Url { get; set; }
         public bool IsMain { get; set; }
     }
