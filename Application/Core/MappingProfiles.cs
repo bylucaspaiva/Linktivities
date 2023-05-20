257ae7e13693f7ecd388eea3c62627d2dabe60b9
Lucas Paiva <lucasp4iva@gmail.com>
2023-05-20 02:51:10 -0300

    feat: return an AttendeeDto

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -14,13 +14,17 @@ public class MappingProfiles : Profile
     public MappingProfiles()
     {
         CreateMap<Activity, Activity>();
+
         CreateMap<Activity, ActivityDTO>()
-            .ForMember(d => d.HostUserName, o => o.MapFrom(s => s.Attendees
-                .FirstOrDefault(x => x.IsHost).AppUser.UserName));
-        CreateMap<ActivityAttendee, Profiles.Profile>()
+            .ForMember(d => d.HostUserName,
+            o => o.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
+        
+        CreateMap<ActivityAttendee, AttendeeDTO>()
             .ForMember(d => d.DisplayName, o => o.MapFrom((s => s.AppUser.DisplayName)))
             .ForMember(d => d.Username, o => o.MapFrom((s => s.AppUser.UserName)))
-            .ForMember(d => d.Bio, o => o.MapFrom((s => s.AppUser.Bio)));
+            .ForMember(d => d.Bio, o => o.MapFrom((s => s.AppUser.Bio)))
+            .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+        
         CreateMap<AppUser, Profiles.Profile>()
             .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
 
