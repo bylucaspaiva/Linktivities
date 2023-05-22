db2e77cef8567ff2be05b1634445e7360e337cd7
Lucas Paiva <lucasp4iva@gmail.com>
2023-05-21 22:52:22 -0300

    feat: add uploadPhoto method

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 import { Activity, ActivityFormValues } from '../models/activity';
 import { store } from '../stores/store';
 import { User, UserFormValues } from '../models/user';
-import { Profile } from '../models/profile';
+import { Photo, Profile } from '../models/profile';
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
     setTimeout(resolve, delay) ;
@@ -93,6 +93,13 @@ const Account = {
 
 const Profiles = {
   get: (username: string) => requests.get<Profile>(`/profiles/${username}`),
+  uploadPhoto: (file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return axios.post<Photo>('photos', formData, {
+      headers: {'Content-type': 'Content-type'}
+    })
+  }
 }
 
 const agent = {
