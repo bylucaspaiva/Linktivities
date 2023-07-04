5357f0603f28ba8cb5db3b91c8c8db920468791c
bylucaspaiva <lucasp4iva@gmail.com>
2023-07-04 11:10:30 -0300

    Feat: add List Followers Handler

---
+++ Application/Followers/List.cs
@@ -0,0 +1,38 @@
+﻿using Application.Profiles;
+using AutoMapper;
+using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Followers
+{
+    internal class List
+    {
+        public class Query : IRequest<List<Profiles.Profile>>
+        {
+            public string Predicate { get; set; }
+            public string Username { get; set; }
+
+        }
+        public class Handler : IRequestHandler<Query, List<Profiles.Profile>>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            Task<List<Profiles.Profile>> IRequestHandler<Query, List<Profiles.Profile>>.Handle(Query request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
