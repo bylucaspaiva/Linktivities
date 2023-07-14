c55e64907711d42939746126f38f1c24f7715fbe
bylucaspaiva <lucasp4iva@gmail.com>
2023-07-14 12:06:43 -0300

    Feat: add followController and mapping configs

--- API/Controllers/FollowController.cs
+++ API/Controllers/FollowController.cs
@@ -8,6 +8,12 @@ public class FollowController : BaseApiController
     [HttpPost("{username}")]
     public async Task<IActionResult> Follow(string username)
     {
-        return HandleResult(await Mediator.Send(new FollowToggle.Command{ TargetUsername = username }));
+        return HandleResult(await Mediator.Send(new FollowToggle.Command { TargetUsername = username }));
+    }
+
+    [HttpGet("{username}")]
+    public async Task<IActionResult> GetFollowings(string username, string predicate)
+    {
+        return HandleResult(await Mediator.Send(new List.Query { Username = username, Predicate = predicate }));
     }
 }
