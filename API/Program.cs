c28f46e625085c48406c056bce6b344fe4793e94
bylucaspaiva <lucasp4iva@gmail.com>
2023-07-24 15:44:54 -0300

    add static files to api

--- API/Program.cs
+++ API/Program.cs
@@ -38,11 +38,14 @@ if (app.Environment.IsDevelopment())
 app.UseCors("CorsPolicy");
 
 app.UseAuthentication();
-
 app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.MapControllers();
 app.MapHub<ChatHub>("/chat");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 
